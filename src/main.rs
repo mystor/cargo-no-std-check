fdc612ff1ff61543d038da988cfdb54fa8a6ea7f
Nika Layzell <nika@thelayzells.com>
2020-01-04 02:23:06 -0500

    mostly-working

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,9 @@
 use anyhow::Result;
 use std::env;
-use std::io;
 use tracing::Level;
+use std::io;
 
 fn main() -> Result<()> {
-    tracing_subscriber::fmt::Subscriber::builder()
-        .with_writer(io::stderr)
-        .with_max_level(Level::TRACE)
-        .compact()
-        .init();
     cargo_no_std_check::run(env::args().skip(1).collect())?;
     Ok(())
 }
